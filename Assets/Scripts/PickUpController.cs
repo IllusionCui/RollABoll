cf64fe27e7079180b410e8d60b4d34b76a01fcb1
illusion <657488847@qq.com>
2015-08-14 17:17:48 +0800

    unity tutoral

---
+++ Assets/Scripts/PickUpController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpController : MonoBehaviour {
+	public Vector3 rotate;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rotate*Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		setCount (0);
+		winText.gameObject.SetActive (false);
+	}
+
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("PickUp")) {
+			Debug.Log("PlayerController\tPickUp");
+			other.gameObject.SetActive(false);
+			setCount (count + 1);
+			if (count == 12) {
+				winText.gameObject.SetActive (true);
+			}
+		}
+	}
+
+	void setCount(int value) {
+		count = value;
+		countText.text = "Score:\t" + count.ToString ();
+	}
+}
